530e18f5506c60a8446a32b3a550a7751d94d564
premangadiki <87991780+premangadiki@users.noreply.github.com>
2022-01-14 19:51:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+scoreRightWrist=0;
+scoreLeftWrist=0;
 
 function setup(){
     canvas = createCanvas(600 , 500);
@@ -22,7 +24,9 @@ function gotPoses(results){
     if(results.length > 0){
 
         scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         console.log("Score of leftWrist =" + scoreLeftWrist);
+        console.log("Score of rightWrist =" + scoreRightWrist);
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -31,6 +35,7 @@ function gotPoses(results){
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX =" + rightWristX + "rightWristY =" + rightWristY);
+
     }
 }
 
@@ -49,6 +54,35 @@ function draw(){
      document.getElementById("volume").innerHTML = "Volume =" + volume;
      song.setVolume(volume);
     }
+
+    if(scoreRightWrist > 0.2){
+
+    circle(rightWristX , rightWristY , 20);
+     
+    if(rightWristY > 0 && rightWristY <= 100){
+        document.getElementByID("speed").innerHTML = "Speed of the song = 0.5x";
+        song.rate(0.5);
+    }
+
+  else if(rightWristY > 100 && rightWristY <= 200){
+        document.getElementByID("speed").innerHTML = "Speed of the song = 1.0x";
+        song.rate(1);
+    }
+    else if(rightWristY > 200 && rightWristY <= 300){
+        document.getElementByID("speed").innerHTML = "Speed of the song = 1.5x";
+        song.rate(1.5);
+    }
+
+    else if(rightWristY > 300 && rightWristY <= 400){
+        document.getElementByID("speed").innerHTML = "Speed of the song = 2.0x";
+        song.rate(2);
+    }
+
+    else if(rightWristY > 400 && rightWristY <= 500){
+        document.getElementByID("speed").innerHTML = "Speed of the song = 2.5x";
+        song.rate(2.5);
+    }
+}
 }
  
 function preload(){
